51d899d15bc8b8e72f37f2c989f43c7c2e0b69d0
Sameer Anis <159227804+DigitalDreamweaver@users.noreply.github.com>
2024-03-27 21:19:54 +0500

    Create_Atm_Project

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+let myBalance = 10000; //Dollar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your pin"
+    },
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Welcome to your Account");
+    let operationAns = await inquirer.prompt([{
+            name: "operation",
+            message: "Please Select Option",
+            type: "list",
+            choices: ["Withdraw", "Balance Inquiry",]
+        }]);
+    if (operationAns.operation === "Withdraw") {
+        let amountAns = await inquirer.prompt([{
+                name: "amount",
+                message: "Enter Amount",
+                type: "number"
+            }]);
+        myBalance -= amountAns.amount;
+        console.log("Your Remaing Balance is:" + " " + myBalance);
+    }
+    else if (operationAns.operation === "Balance Inquiry") {
+        console.log("Your Balance is:" + " " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect Pin Number");
+}
+;
